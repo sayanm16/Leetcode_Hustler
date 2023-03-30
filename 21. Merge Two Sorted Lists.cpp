4f6785906889109af934d672f5757e6b182368d6
Sayan Mukherjee <98234012+sayanm16@users.noreply.github.com>
2023-03-30 08:28:50 +0530

    created

---
+++ 21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr) return list2;
+        else if (list2 == nullptr) return list1;
+        else if (list1->val < list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = mergeTwoLists(list2->next, list1);
+            return list2;
+        }
+    }
+};
